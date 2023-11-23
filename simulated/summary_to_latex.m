33814d594507e441b53c5fd10d8fc3815c8eb96c
Anderson Winkler <andersonwinkler@gmail.com>
2023-11-23 10:05:10 -0600

    changed dep from 0.8 to 0.25

---
+++ simulated/summary_to_latex.m
@@ -0,0 +1,59 @@
+[repodir,~,~] = fileparts(mfilename('fullpath'));
+
+% Load the table, sort
+T = readtable(fullfile(repodir,'summary_results.csv'));
+T = sortrows(T,{'FDRmethod','scenario'},{'ascend','ascend'});
+
+% Select columns of interest, convert to percentages
+Tx = T(:,14:end-1);
+Tx = table2array(Tx)*100;
+
+% % Way 1: BH at the top, BKY at the bottom
+% for r = 1:size(Tx,1)
+%     if r > 10
+%         scn = r-10;
+%     else
+%         scn = r;
+%     end
+%     fprintf('\\textsc{%s}',lower(integer2roman(scn)));
+%     fprintf(' & %0.1f \\scalebox{.7}[1.0]{(%0.1f--%0.1f)}', Tx(r,:));
+%     fprintf(' \\\\\n');
+% end
+
+% Way 2: BH on the left, BKY on the right
+% Rearrange the table
+Ty = [];
+Ty( 1:10, 1:12) = Tx( 1:10,1:12);
+Ty( 1:10,13:24) = Tx(11:20,1:12);
+Ty(11:20, 1:12) = Tx( 1:10,13:24);
+Ty(11:20,13:24) = Tx(11:20,13:24);
+Ty(21:30, 1:12) = Tx( 1:10,25:36);
+Ty(21:30,13:24) = Tx(11:20,25:36);
+for r = 1:size(Ty,1)
+    if r > 20
+        scn = r-20;
+    elseif r > 10
+        scn = r-10;
+    else
+        scn = r;
+    end
+    fprintf('\\textsc{%s}',lower(integer2roman(scn)));
+    fprintf(' & %0.1f \\scalebox{.7}[1.0]{(%0.1f--%0.1f)}', Ty(r,:));
+    fprintf(' \\\\\n');
+end
+
+% =========================================================================
+function roman = integer2roman(n)
+if (n < 1 || n > 3999)
+    error('Input must be an integer between 1 and 3999.');
+end
+val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+syms = {'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'};
+roman = '';
+for i = 1:length(val)
+    while n >= val(i)
+        n = n - val(i);
+        roman = [roman, syms{i}];
+    end
+end
+end
