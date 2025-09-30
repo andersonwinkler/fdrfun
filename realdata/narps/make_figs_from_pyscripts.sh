c2809cc91dbde88cd52815dfd4fdb0a9011b1d33
Anderson Winkler <andersonwinkler@gmail.com>
2025-09-30 00:07:43 -0500

    BB2014 for real data

---
+++ realdata/narps/make_figs_from_pyscripts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for x in *.py ; do /Applications/MRIcroGL.app/Contents/MacOS/MRIcroGL ${x} ; done
--- realdata/narps/run_for_real.m
+++ realdata/narps/run_for_real.m
@@ -14,8 +14,8 @@ switch lower(FDRmethod)
 end
 
 % Read the data
-T      = palm_miscread('narps-4965_9U7M-hypo1_unthresh.nii',true);
-M      = palm_miscread('narps-4965_9U7M-mask.nii',true);
+T      = palm_miscread('narps-4965_9U7M-hypo1_unthresh.nii',false);
+M      = palm_miscread('narps-4965_9U7M-mask.nii',false);
 mask   = logical(M.data);
 tstats = T.data(mask);
 nV     = numel(tstats);
@@ -28,11 +28,11 @@ idxneg = ~idxpos;
 df           = 107;
 pvals0       = tcdf(tstats,df,'upper');
 T.data(mask) = -log10(pvals0);
-T.filename   = 'narps-4965_9U7M-hypo1_unc_pvals_mappos.nii';
+T.filename   = 'narps-4965_9U7M-hypo1_unc_pvals_mappos';
 palm_miscwrite(T);
 pvals1       = tcdf(tstats,df); % 1-p, to avoid losing precision
 T.data(mask) = -log10(pvals1);
-T.filename   = 'narps-4965_9U7M-hypo1_unc_pvals_mapneg.nii';
+T.filename   = 'narps-4965_9U7M-hypo1_unc_pvals_mapneg';
 palm_miscwrite(T);
 J.pposthr    = -log10(alpha/2);
 J.pnegthr    = -log10(alpha/2);
@@ -44,10 +44,10 @@ writejson(J,'narps-4965_9U7M-hypo1_unc_pvals.json');
 [thrcan0,~,adjcan0] = fdrfun(pvals0);
 [thrcan1,~,adjcan1] = fdrfun(pvals1);
 T.data(mask)  = -log10(adjcan0);
-T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_canonical_mappos.nii',fdrstr);
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_canonical_mappos',fdrstr);
 palm_miscwrite(T);
 T.data(mask)  = -log10(adjcan1);
-T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_canonical_mapneg.nii',fdrstr);
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_canonical_mapneg',fdrstr);
 palm_miscwrite(T);
 J.pposthr     = -log10(thrcan0);
 J.pnegthr     = -log10(thrcan1);
@@ -56,12 +56,12 @@ J.znegthr     = -tinv( thrcan1,df);
 writejson(J,sprintf('narps-4965_9U7M-hypo1_%s_canonical.json',fdrstr));
 
 % COMBINED
-[thrcom,~,fdrcom] = fdrfun([pvals0;pvals1]);
-T.data(mask)  = -log10(fdrcom(1:nV));
-T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_combined_mappos.nii',fdrstr);
+[thrcom,~,adjcom] = fdrfun([pvals0;pvals1]);
+T.data(mask)  = -log10(adjcom(1:nV));
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_combined_mappos',fdrstr);
 palm_miscwrite(T);
-T.data(mask)  = -log10(fdrcom(nV+1:end));
-T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_combined_mapneg.nii',fdrstr);
+T.data(mask)  = -log10(adjcom(nV+1:end));
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_combined_mapneg',fdrstr);
 palm_miscwrite(T);
 J.pposthr     = -log10(thrcom);
 J.pnegthr     = -log10(thrcom);
@@ -71,9 +71,9 @@ writejson(J,sprintf('narps-4965_9U7M-hypo1_%s_combined.json',fdrstr));
 
 % TWO-TAILED
 pvals2 = 2*tcdf(abs(tstats),df,'upper');
-[thrtwo,~,fdrtwo] = fdrfun(pvals2);
-T.data(mask)  = -log10(fdrtwo);
-T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_twotail.nii',fdrstr);
+[thrtwo,~,adjtwo] = fdrfun(pvals2);
+T.data(mask)  = -log10(adjtwo);
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_twotail',fdrstr);
 palm_miscwrite(T);
 J.pposthr     = -log10(thrtwo);
 J.pnegthr     = -log10(thrtwo);
@@ -82,14 +82,50 @@ J.znegthr     = -tinv( thrtwo,df);
 writejson(J,sprintf('narps-4965_9U7M-hypo1_%s_twotail.json',fdrstr));
 
 % SPLIT + TWO-TAILED
-fdrspl = ones(size(pvals0));
-[thrspl0,~,fdrspl(idxpos)] = fdrfun(pvals2(idxpos));
-[thrspl1,~,fdrspl(idxneg)] = fdrfun(pvals2(idxneg));
-T.data(mask)  = -log10(fdrspl);
-T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_split2tail.nii',fdrstr);
+adjspl = ones(size(pvals0));
+[thrspl0,~,adjspl(idxpos)] = fdrfun(pvals2(idxpos));
+[thrspl1,~,adjspl(idxneg)] = fdrfun(pvals2(idxneg));
+T.data(mask)  = -log10(adjspl);
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_split2tail',fdrstr);
 palm_miscwrite(T);
 J.pposthr     = -log10(thrspl0);
 J.pnegthr     = -log10(thrspl1);
 J.zposthr     = -tinv( thrspl0,df);
 J.znegthr     = -tinv( thrspl1,df);
 writejson(J,sprintf('narps-4965_9U7M-hypo1_%s_split2tail.json',fdrstr));
+
+% CANONICAL + BB2014
+Pset = [pvals0';pvals1'];
+[thrcanbb,~,adjcanbb] = bb2014(Pset,[],fdrfun);
+thrcanbb0 = thrcanbb(1);
+thrcanbb1 = thrcanbb(2);
+adjcanbb0 = adjcanbb(1,:)';
+adjcanbb1 = adjcanbb(2,:)';
+T.data(mask)  = -log10(adjcanbb0);
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_canonicalbb_mappos',fdrstr);
+palm_miscwrite(T);
+T.data(mask)  = -log10(adjcanbb1);
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_canonicalbb_mapneg',fdrstr);
+palm_miscwrite(T);
+J.pposthr     = -log10(thrcanbb0);
+J.pnegthr     = -log10(thrcanbb1);
+J.zposthr     = -tinv( thrcanbb0,df);
+J.znegthr     = -tinv( thrcanbb1,df);
+writejson(J,sprintf('narps-4965_9U7M-hypo1_%s_canonicalbb.json',fdrstr));
+
+% SPLIT + TWO-TAILED + BB2014
+adjsplbb = ones(size(pvals0));
+Pset = {pvals2(idxpos), pvals2(idxneg)};
+[tmpthr,~,tmpadj] = bb2014(Pset,[],fdrfun);
+thrsplbb0 = tmpthr{1};
+thrsplbb1 = tmpthr{2};
+adjsplbb(idxpos) = tmpadj{1};
+adjsplbb(idxneg) = tmpadj{2};
+T.data(mask)  = -log10(adjsplbb);
+T.filename    = sprintf('narps-4965_9U7M-hypo1_%s_split2tailbb',fdrstr);
+palm_miscwrite(T);
+J.pposthr     = -log10(thrsplbb0);
+J.pnegthr     = -log10(thrsplbb1);
+J.zposthr     = -tinv( thrsplbb0,df);
+J.znegthr     = -tinv( thrsplbb1,df);
+writejson(J,sprintf('narps-4965_9U7M-hypo1_%s_split2tailbb.json',fdrstr));
